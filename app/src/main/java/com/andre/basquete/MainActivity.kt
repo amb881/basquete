360bcf825670dc9a539c9dedb48ebc28adbb320f
AndreBaptista <andre.m.b.1@hotmail.com>
2021-01-25 21:41:06 +0000

    Correção da implementação da firebase Criação do Registo e Login

--- app/src/main/java/com/andre/basquete/MainActivity.kt
+++ app/src/main/java/com/andre/basquete/MainActivity.kt
@@ -2,21 +2,58 @@ package com.andre.basquete
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val haveAccount: TextView = findViewById<TextView>(R.id.haveAccount_textView)
-        haveAccount.setOnClickListener{
-            //abrir página do login
+        register_button_register.setOnClickListener {
+            performRegister()
+        }
+
+        //Ir para a página de login
+        haveAccount_textView.setOnClickListener {
+            //redirecionar para a página de login
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
 
-
     }
+
+    private fun performRegister() {
+        register_button_register.setOnClickListener {
+
+            val email = email_editText_register.text.toString()
+            val password = password_editText_register.text.toString()
+
+            if(email.isEmpty() || password.isEmpty()){
+                Toast.makeText(this, "Enter text in all fields", Toast.LENGTH_SHORT ).show()
+                return@setOnClickListener
+            }
+
+            Log.d("caralho", "email: $email")
+            Log.d("caralho", "pass: $password")
+
+            //Firebase Authentication
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener {
+                        if(!it.isSuccessful) return@addOnCompleteListener
+                    }
+                    .addOnFailureListener{
+                        Log.d("Main", "Failed to create user: ${it.message}")
+                        Toast.makeText(this, "Failed to create user: ${it.message}", Toast.LENGTH_SHORT ).show()
+                    }
+
+        }
+    }
+
 }
